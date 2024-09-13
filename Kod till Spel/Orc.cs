e0b6b4d82fddb79866eec75887914a9ec3e3804d
HAELGEE <helge1337@live.com>
2024-09-13 14:50:54 +0200

    update

--- Kod till Spel/Orc.cs
+++ Kod till Spel/Orc.cs
@@ -11,113 +11,20 @@ using System.Runtime.Intrinsics.Arm;
 using System.Xml.Linq;
 
 namespace Kod_till_Spel;
+
 public class OrcBase
 {
-    public void Green(string value)
+    
+    public void LevelCheck(Hero hero)
     {
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.Write(value);
-        Console.ResetColor();
-    }
-    public void Red(string value)
-    {
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.Write(value);
-        Console.ResetColor();
-    }
-    public void Cyan(int value)
-    {
-        Console.ForegroundColor = ConsoleColor.Cyan;
-        Console.Write(value);
-        Console.ResetColor();
-    }
+        Random random = new Random();
 
-    public string name { get; set; }
-    public int level { get; set; } = 1;
-    public double experience { get; set; } = 0;
-    public int hp { get; set; } = 10;
-    public int styrka { get; set; } = 1;
-    public int agility { get; set; } = 1;
-    public int stamina { get; set; } = 1;
-    public int intelligence { get; set; } = 0;
-    public int mana { get; set; } = 0;
-    public int charm { get; set; } = 0;    
-    public double damage { get; set; }
-    public double speed { get; set; }
-    public double armor { get; set; }
-
-    public Random random = new Random();
-
-    public int Attack(Hero hero)
-    {
-        int minDamage = 1;
-        int maxDamage = 4;
-        minDamage += (int)damage - (int)hero.armor;
-        maxDamage += (int)damage - (int)hero.armor;
-        int value = random.Next(minDamage, maxDamage);
-        Red(name);                  //lägger in färgen RÖD på orc
-        Console.Write(" gjorde ");
-        Cyan(value);                //Lägger till färgen CYAN på DMG
-        Console.WriteLine(" skada.");     
-        return value;
-    }
-
-    public virtual void GenerateName()
-    {
-        this.name = "Orc-" + random.Next(1, 3340);
-    }
-}
-
-public class Orc : OrcBase
-{
-    public Orc()
-    {
-        this.damage = 2;
-        this.speed = 1;
-        this.armor = 1;
-        GenerateName();
-    }
-
-    public virtual int Attack(Hero hero)
-    {
-        return base.Attack(hero);      // Specifik attack för Orc        
-    }
-}
-public class Shaman : OrcBase
-{
-    public Shaman()
-    {
-        this.damage = 3;
-        this.speed = 0.9;
-        this.armor = 1;
-        this.name = "Shaman-" + random.Next(1, 3340);
-    }
-
-    public virtual int Attack(Hero hero)
-    {
-        return base.Attack(hero);   // Specifik attack för Shaman        
-    }
-}
-
-public class Grunt : OrcBase
-{
-    public Grunt()
-    {
-        this.damage = 1;
-        this.speed = 0.5;
-        this.armor = 3;
-        this.name = "Grunt-" + random.Next(1, 3340);
-    }
-
-    public virtual int Attack(Hero hero)
-    {
-        return base.Attack(hero);      // Specifik attack för Grunt        
-    }
-
-    private void LevelCheck(Hero hero)
-    {
         int levelOver = 0;
         int levelUnder = 1;
+        if (levelUnder < 1)
+        {
+            levelUnder = 1;
+        }
 
         if (hero.level >= this.level)
         {
@@ -173,4 +80,116 @@ public class Grunt : OrcBase
             }
         }
     }
+    public void Green(string value)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.Write(value);
+        Console.ResetColor();
+    }
+    public void Red(string value)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write(value);
+        Console.ResetColor();
+    }
+    public void Cyan(int value)
+    {
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.Write(value);
+        Console.ResetColor();
+    }
+
+    public string name { get; set; }
+    public int level { get; set; } = 1;
+    public double experience { get; set; } = 0;
+    public int hp { get; set; } = 10;
+    public int styrka { get; set; } = 1;
+    public int agility { get; set; } = 1;
+    public int stamina { get; set; } = 1;
+    public int intelligence { get; set; } = 0;
+    public int mana { get; set; } = 0;
+    public int charm { get; set; } = 0;
+    public double damage { get; set; }
+    public double speed { get; set; }
+    public double armor { get; set; }
+
+    public Random random = new Random();
+
+    public int Attack(Hero hero)
+    {
+        int minDamage = 1;
+        int maxDamage = 4;
+        minDamage += (int)damage - (int)hero.armor;
+        maxDamage += (int)damage - (int)hero.armor;
+        int value = random.Next(minDamage, maxDamage);
+        Red(name);                  //lägger in färgen RÖD på orc
+        Console.Write(" gjorde ");
+        Cyan(value);                //Lägger till färgen CYAN på DMG
+        Console.WriteLine(" skada.");
+        return value;
+    }
+
+    public virtual void GenerateName()
+    {
+        this.name = "Orc-" + random.Next(1, 3340);
+    }
 }
+
+public class Orc : OrcBase
+{
+    public Orc()
+    {
+        this.damage = 2;
+        this.speed = 1;
+        this.armor = 1;
+        GenerateName();
+        Hero hero = new Hero();
+        LevelCheck(hero);
+    }
+
+    public virtual int Attack(Hero hero)
+    {
+
+        return base.Attack(hero);      // Specifik attack för Orc        
+    }
+}
+public class Shaman : OrcBase
+{
+    public Shaman()
+    {
+        this.damage = 3;
+        this.speed = 0.9;
+        this.armor = 1;
+        this.name = "Shaman-" + random.Next(1, 3340);
+        Hero hero = new Hero();
+        LevelCheck(hero);
+    }
+
+    public virtual int Attack(Hero hero)
+    {
+
+        return base.Attack(hero);   // Specifik attack för Shaman        
+    }
+}
+
+public class Grunt : OrcBase
+{
+    public Grunt()
+    {
+        this.damage = 1;
+        this.speed = 0.5;
+        this.armor = 3;
+        this.name = "Grunt-" + random.Next(1, 3340);
+        Hero hero = new Hero();
+        LevelCheck(hero);
+    }
+
+    public virtual int Attack(Hero hero)
+    {
+
+        return base.Attack(hero);      // Specifik attack för Grunt        
+    }
+}
+
+
+
